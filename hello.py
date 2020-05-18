1014bb4677b783678a0f6d1dbc3f698db759478d
Mickaël <mickael.caro@sonarsource.com>
2020-05-18 11:24:12 +0200

    Add codesmell

--- hello.py
+++ hello.py
@@ -1,1 +1,15 @@
 print("Hello World")
+
+def __mul__(self, other, unexpected):  # Noncompliant. Too many parameters
+     return 42
+
+def __add__(self):  # Noncompliant. Missing one parameter
+    return 42
+    
+# TODO
+
+
+class SomeClass:
+    lookUp = false
+    def lookup():       # Non-compliant; method name differs from field name only by capitalization
+        pass
