e15700ccc05c2aea9c7dbda464b624b5a826c679
mickael-caro-sonarsource <mickael.caro@sonarsource.com>
2021-05-21 10:40:42 +0200

    Update choices.py

--- python/choices.py
+++ python/choices.py
@@ -27,6 +27,16 @@ def _clean_many(prefix):
         keys = ["%s-%s" %(prefix, settings.LANGUAGE_CODE)]
     cache.delete_many(keys)
 
+def _clean_many(prefix):
+    from django.core.cache import cache
+    keys = []
+    if settings.USE_I18N:
+        for lang in [language[0] for language in settings.LANGUAGES]:
+            keys.append("%s-%s" %(prefix, lang))
+    else:
+        keys = ["%s-%s" %(prefix, settings.LANGUAGE_CODE)]
+    cache.delete_many(keys)
+
 
 def clean_site_choices_cache(sender, **kwargs):
     _clean_many(get_cms_setting('SITE_CHOICES_CACHE_KEY'))
