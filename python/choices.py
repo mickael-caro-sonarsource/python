f76f58ceafa112517962a4399727b3f541e83607
mickael-caro-sonarsource <mickael.caro@sonarsource.com>
2021-05-21 10:47:28 +0200

    Update choices.py

--- python/choices.py
+++ python/choices.py
@@ -2,7 +2,12 @@
 from django.conf import settings
 
 from cms.utils.conf import get_cms_setting
+def _site_cache_key(lang):
+    return "%s-%s" %(get_cms_setting('SITE_CHOICES_CACHE_KEY'), lang)
 
+
+def _page_cache_key(lang):
+    return "%s-%s" %(get_cms_setting('PAGE_CHOICES_CACHE_KEY'), lang)
 
 def _site_cache_key(lang):
     return "%s-%s" %(get_cms_setting('SITE_CHOICES_CACHE_KEY'), lang)
