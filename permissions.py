913fb97ee43a6f81c016972b32b153736208ba4b
mickael-caro-sonarsource <mickael.caro@sonarsource.com>
2021-05-26 14:17:59 +0200

    Update permissions.py

--- permissions.py
+++ permissions.py
@@ -36,7 +36,18 @@ def get_permission_cache(user, key):
     from django.core.cache import cache
     return cache.get(get_cache_key(user, key), version=get_cache_permission_version())
 
-
+def set_permission_cache(user, key, value):
+    """
+    Helper method for storing values in cache. Stores used keys so
+    all of them can be cleaned when clean_permission_cache gets called.
+    """
+    from django.core.cache import cache
+    # store this key, so we can clean it when required
+    cache_key = get_cache_key(user, key)
+    cache.set(cache_key, value,
+              get_cms_setting('CACHE_DURATIONS')['permissions'],
+              version=get_cache_permission_version())
+    
 def set_permission_cache(user, key, value):
     """
     Helper method for storing values in cache. Stores used keys so
