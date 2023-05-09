c39678beeefb9cb658859ebafe9194be1118d809
Mickaël Caro <mickael.caro@sonarsource.com>
2023-05-09 10:12:32 +0200

    Update permissions.py

--- permissions.py
+++ permissions.py
@@ -10,14 +10,11 @@ PERMISSION_KEYS = [
     'publish_page', 'view_page',
 ]
 
-
-
-PERMISSION_KEYS_2 = [
+PERMISSION_KEYS_3 = [
     'add_page', 'change_page', 'change_page_advanced_settings',
     'change_page_permissions', 'delete_page', 'move_page',
     'publish_page', 'view_page',
 ]
-
 
 def get_cache_key(user, key):
     username = getattr(user, get_user_model().USERNAME_FIELD)
